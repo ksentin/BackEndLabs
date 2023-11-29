ddfb2c6e1a8d1a10a0858c591b13e616448bf8fa
ksentin <ksenpati@gmail.com>
2023-11-29 12:57:18 +0200

    Added configuration for database

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py', silent=True)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from myapp.models import User, Category, Record
 
 import myapp.views
