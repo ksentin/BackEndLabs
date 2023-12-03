fceced42cd6eb2bc716f26bcee935f244e84000d
ksentin <ksenpati@gmail.com>
2023-12-03 11:07:00 +0200

    Added Internal Database URL from render

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,7 +5,8 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://db_wyuf_user:Q0oBcLIpwajVcrywOOLd6UN8cqUOAJRu@dpg-clloj01fb9qs73938cpg-a.oregon-postgres.render.com/db_wyuf'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
