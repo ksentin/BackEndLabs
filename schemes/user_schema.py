73f95584a5ddfaba8b004e43674c391953ddfef6
ksentin <ksenpati@gmail.com>
2023-12-14 20:33:35 +0200

    Edit models for registration

--- schemes/user_schema.py
+++ schemes/user_schema.py
@@ -3,4 +3,5 @@ from marshmallow import Schema, fields, validate
 class UserSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
-    default_currency_id = fields.Integer(required=True)
+    default_currency_name = fields.String(required=True)
+    password = fields.String(required=True)
